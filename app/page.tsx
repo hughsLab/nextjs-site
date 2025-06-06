0e86526774b1b7d68a38dba984a7944b2251565a
Chessdog68 <129558843+Chessdog68@users.noreply.github.com>
2025-06-06 12:22:40 +1000

    u

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          I'm building projects and a start up my free time{" "}
+          I'm building projects and a start up in my free time{" "}
           <Link
             target="_blank"
             href="https://www.youtube.com/watch?v=4wN1Sxfbe3M&t=155s"
