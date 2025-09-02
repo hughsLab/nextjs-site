8eed67073bd05738651c7bbe279da0540b5f6363
Hugh Mathys <hughmathys6@gmail.com>
2025-09-02 20:53:55 +1000

    projects link fixed, uptash link fixed

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import Particles from "./components/particles";
 
 const navigation = [
-  { name: "Projects", href: "/contact" },
+  { name: "Projects", href: "/project" },
   { name: "Contact", href: "/contact" },
 ];
 
@@ -35,7 +35,7 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          I'm building-TESTING projects and a start up in my free time{" "}
+          I'm building projects and a start up in my free time{" "}
           <Link
             target="_blank"
             href="https://morphic-ai-answer-engine-generative-liart.vercel.app/"
